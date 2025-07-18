edd8092aa85fb4abc9d9ba12b75202ac296017fb
Surya Prakash <156089547+Surya-Prakash-S-hub@users.noreply.github.com>
2025-07-18 21:30:33 +0530

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@ var swiper = new Swiper(".mySwiper", {
   spaceBetween: 30,
   centeredSlides: true,
   autoplay: {
-    delay: 5000,
+    delay: 3000,
     disableOnInteraction: false,
   },
   pagination: {
